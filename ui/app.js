eac443e27518229d3a630404852400cfa55e1518
Willy Kloucek <wkloucek@owncloud.com>
2021-05-20 16:23:06 +0200

    add more extensions

--- ui/app.js
+++ ui/app.js
@@ -15,12 +15,112 @@ const appInfo = {
       icon: 'x-office-document'
     },
     {
+      extension: 'ott',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
       extension: 'ods',
       routeName: 'wopi-edit',
       icon: 'x-office-document'
     },
     {
+      extension: 'ots',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
       extension: 'odp',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'otp',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'odg',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'otg',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'doc',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'dot',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'xls',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'xlt',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'xlm',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'ppt',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'pot',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'pps',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'vsd',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'dxf',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'wmf',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'cdr',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'pages',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'number',
+      routeName: 'wopi-edit',
+      icon: 'x-office-document'
+    },
+    {
+      extension: 'key',
       routeName: 'wopi-edit',
       icon: 'x-office-document'
     }
